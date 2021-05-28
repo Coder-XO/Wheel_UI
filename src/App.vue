d59eacdacd35e6333348cf1fc4e9fb294d949f37
Coder-XO <coderless@gmail.com>
2021-05-28 09:59:50 +0800

    初始化路由

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>hi</div>
-  <Frank/>
+  <div>导航栏 |
+    <router-link to="/">Frank</router-link>&nbsp;
+    <router-link to="/xxx">Frank2</router-link>
+  </div>
+  <router-view/>
 </template>
 
 <script>
-import Frank from './components/Frank.vue'
-
 export default {
-  name: 'App',
-  components: {
-    Frank
-  }
+  name: 'App'
 }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,19 @@ import App from './App.vue';
 import './index.css';
 import {createWebHashHistory, createRouter} from 'vue-router';
 import Frank from './components/Frank.vue';
+import Frank2 from './components/Frank2.vue';
 
-const history = createWebHashHistory();
-const router = createRouter({
-    history: history,
-    routes: [
-        {
-            path: '/', component: Frank
-        }
-    ]
+const history = createWebHashHistory();   // hash型路由
+const router = createRouter({   // 创建 router
+  history: history,
+  routes: [
+    {
+      path: '/', component: Frank
+    },
+    {
+      path: '/xxx', component: Frank2
+    }
+  ]
 });
 const app = createApp(App);
 app.use(router);
