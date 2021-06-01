f1d9eaad8558e29619d7fb4e878e4f6c0a847c82
Coder-XO <coderless@gmail.com>
2021-06-01 10:25:25 +0800

    switch组件按钮hover之后右滚

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,7 @@ export default {
     const width = document.documentElement.clientWidth;   // 获取页面宽度判断当前设备，控制aside在pc端默认显示
     const menuVisible = ref(width <= 500 ? false : true);  // pc 或者 手机 是否显示 aside
     provide('menuVisible', menuVisible);   // 标记后代都可以使用
-    router.afterEach(() => {
-      console.log('fuck');
+    router.afterEach(() => {  //  利用导航钩子设置aside可见性
       if (width <= 500) {
         menuVisible.value = false;
       }
