bc0e41f11584bbb71ae290724d65e52c926158db
Coder-XO <coderless@gmail.com>
2021-06-04 09:45:41 +0800

    Dialog组件创建ææ完毕

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,16 @@
 <template>
-  <p>Dialog</p>
+  <div>Dialog 示例</div>
+  <h1>示例1</h1>
+  <Dialog></Dialog>
 </template>
 
 <script>
+import Dialog from "../lib/Dialog.vue";
+
 export default {
-  name: "DialogDemo"
+  components: {
+    Dialog
+  }
 }
 </script>
 
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="dialog-overlay"></div>
+  <div class="dialog_wrapper">
+    <header>标题</header>
+    <main>
+      <p>第一行字</p>
+      <p>第二行字</p>
+    </main>
+    <footer>
+
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from './Button.vue';
+
+export default {
+  components: {
+    Button
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
