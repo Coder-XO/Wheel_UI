fe810ab3ade297151a7add15cbdebf76a25603df
Coder-XO <coderless@gmail.com>
2021-06-04 11:12:15 +0800

    Dialog支持自定义标题和内容

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -5,16 +5,15 @@
     <div class="gulu-dialog-wrapper">
       <div class="gulu-dialog">
         <header>
-          标题
+          <slot name="title"/>
           <span @click="close" class="gulu-dialog-close"></span>
         </header>
         <main>
-          <p>第一行字</p>
-          <p>第二行字</p>
+          <slot name="content"/>
         </main>
         <footer>
-          <Button level="main">OK</Button>
-          <Button>Cancel</Button>
+          <Button level="main" @click="ok">OK</Button>
+          <Button @click="cancel">Cancel</Button>
         </footer>
       </div>
     </div>
