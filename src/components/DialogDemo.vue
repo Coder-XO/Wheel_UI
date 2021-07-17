a553bb85ad7e5e955b70e2a38ad7de858f8c7a39
Coder-XO <coderless@gmail.com>
2021-07-17 20:47:32 +0800

    完成一句话打开dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,10 +2,7 @@
   <div>Dialog 示例</div>
   <h1>示例1</h1>
   <Button @click="toggle">toggle</Button>
-  <Dialog v-model:visible="x"
-          :close-onclick-overlay="false"
-          :ok="f1"
-          :cancel="f2">
+  <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
     <template v-slot:content>
       <strong>hi</strong>
       <div>hi2</div>
@@ -14,35 +11,53 @@
       <strong>加粗的标题</strong>
     </template>
   </Dialog>
+  <h1>示例2</h1>
+  <Button @click="showDialog">show</Button>
 </template>
 
 <script lang="ts">
-import Dialog from '../lib/Dialog.vue';
-import Button from '../lib/Button.vue';
-import {ref} from 'vue';
-
+import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import {
+  ref,
+  h
+} from 'vue'
+import {
+  openDialog
+} from '../lib/openDialog'
 export default {
   components: {
     Dialog,
     Button
   },
   setup() {
-    const x = ref(false);
+    const x = ref(false)
     const toggle = () => {
-      x.value = !x.value;
-    };
+      x.value = !x.value
+    }
     const f1 = () => {
-      return false;    // 点击ok 的阻止关闭
-    };
-    const f2 = () => {
-    };
+      return false
+    }
+    const f2 = () => {}
+    const showDialog = () => {
+      openDialog({
+        title: h('strong', {}, '标题'),
+        content: '你好',
+        ok() {
+          console.log('ok')
+        },
+        cancel() {
+          console.log('cancel')
+        }
+      })
+    }
     return {
-      x, toggle, f1, f2
-    };
+      x,
+      toggle,
+      f1,
+      f2,
+      showDialog
+    }
   }
-};
+}
 </script>
-
-<style scoped>
-
-</style>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,16 +1,15 @@
 <template>
   <template v-if="visible">
     <Teleport to="body">
-      <div class="gulu-dialog-overlay"
-           @click="onClickOverlay"></div>
+      <div class="gulu-dialog-overlay" @click="onClickOverlay"></div>
       <div class="gulu-dialog-wrapper">
         <div class="gulu-dialog">
           <header>
-            <slot name="title"/>
+            <slot name="title" />
             <span @click="close" class="gulu-dialog-close"></span>
           </header>
           <main>
-            <slot name="content"/>
+            <slot name="content" />
           </main>
           <footer>
             <Button level="main" @click="ok">OK</Button>
@@ -23,15 +22,14 @@
 </template>
 
 <script lang="ts">
-import Button from './Button.vue';
-
+import Button from "./Button.vue";
 export default {
   props: {
     visible: {
       type: Boolean,
       default: false
     },
-    closeOnclickOverlay: {   // 是否点击遮罩层关闭
+    closeOnClickOverlay: {
       type: Boolean,
       default: true
     },
@@ -47,27 +45,28 @@ export default {
   },
   setup(props, context) {
     const close = () => {
-      context.emit('update:visible', false);
-    };
+      context.emit('update:visible', false)
+    }
     const onClickOverlay = () => {
-      if (props.closeOnclickOverlay) {
-        close();
+      if (props.closeOnClickOverlay) {
+        close()
       }
-    };
+    }
     const ok = () => {
-      if (props.ok?.() !== false) {  // 语法糖 相当于props.ok && props.ok() !== false
-        close();
+      if (props.ok?.() !== false) {
+        close()
       }
-    };
+    }
     const cancel = () => {
-      context.emit('cancel');
-      close();
-    };
+      context.emit('cancel')
+      close()
+    }
     return {
       close,
+      onClickOverlay,
       ok,
       cancel
-    };
+    }
   }
 };
 </script>
@@ -75,14 +74,12 @@ export default {
 <style lang="scss">
 $radius: 4px;
 $border-color: #d9d9d9;
-
 .gulu-dialog {
   background: white;
   border-radius: $radius;
   box-shadow: 0 0 3px fade_out(black, 0.5);
   min-width: 15em;
   max-width: 90%;
-
   &-overlay {
     position: fixed;
     top: 0;
@@ -92,7 +89,6 @@ $border-color: #d9d9d9;
     background: fade_out(black, 0.5);
     z-index: 10;
   }
-
   &-wrapper {
     position: fixed;
     left: 50%;
@@ -100,8 +96,7 @@ $border-color: #d9d9d9;
     transform: translate(-50%, -50%);
     z-index: 11;
   }
-
-  > header {
+  >header {
     padding: 12px 16px;
     border-bottom: 1px solid $border-color;
     display: flex;
@@ -109,24 +104,20 @@ $border-color: #d9d9d9;
     justify-content: space-between;
     font-size: 20px;
   }
-
-  > main {
+  >main {
     padding: 12px 16px;
   }
-
-  > footer {
+  >footer {
     border-top: 1px solid $border-color;
     padding: 12px 16px;
     text-align: right;
   }
-
   &-close {
     position: relative;
     display: inline-block;
     width: 16px;
     height: 16px;
     cursor: pointer;
-
     &::before,
     &::after {
       content: '';
@@ -137,15 +128,12 @@ $border-color: #d9d9d9;
       top: 50%;
       left: 50%;
     }
-
     &::before {
       transform: translate(-50%, -50%) rotate(-45deg);
     }
-
     &::after {
       transform: translate(-50%, -50%) rotate(45deg);
     }
-
   }
 }
 </style>
